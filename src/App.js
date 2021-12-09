aff6c9ecdb4a69b7ae5db3c73e08fd0306c6861b
Sebastian Naranjo <sebas2013n@gmail.com>
2021-12-09 17:15:19 -0500

    Configuracion para la función de los componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import {TodoCounter} from "./TodoCounter";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
 // import './App.css';
 
 const todos = [
-  { text: 'Cortar cebolla', completed: false },
+  { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso', completed: false },
   { text: 'Ver videos', completed: false },
 ];
@@ -12,15 +16,13 @@ function App() {
   return (
     <React.Fragment>
       <TodoCounter />
-      {/* <TodoSerch /> */}
-      <input placeholder='Cebolla' />
-      {/* <TodoList>
+      <TodoSearch />
+      <CreateTodoButton/>
+      <TodoList>
         {todos.map(todo => (
-          <TodoItem />
+          <TodoItem key={todo.text} text={todo.text}/>
         ))}
-      </TodoList> */}
-      {/* <CreateTodoButton /> */}
-      <button>+</button>
+      </TodoList>
     </React.Fragment>
   );
 }
