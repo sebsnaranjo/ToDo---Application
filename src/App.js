b41287cce5dd90f64881c7808cd2809c68668b2b
Sebastian Naranjo <sebas2013n@gmail.com>
2021-12-08 21:41:24 -0500

    Configuración inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {TodoCounter} from "./TodoCounter";
+// import './App.css';
+
+const todos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Tomar el curso', completed: false },
+  { text: 'Ver videos', completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+      {/* <TodoSerch /> */}
+      <input placeholder='Cebolla' />
+      {/* <TodoList>
+        {todos.map(todo => (
+          <TodoItem />
+        ))}
+      </TodoList> */}
+      {/* <CreateTodoButton /> */}
+      <button>+</button>
+    </React.Fragment>
   );
 }
 
